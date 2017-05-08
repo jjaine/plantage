59eb2a73de75ee3a0aed06180603e254a967cc4f
Essi Jukkala <essi.jukkala@aalto.fi>
2017-05-08 16:59:06 +0300

    first assets

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	float x,y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Input.mousePosition.x;
+    	y = Input.mousePosition.y;
+	}
+	void OnMouseDrag(){
+    	transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,9.0f));
+	}
+}
