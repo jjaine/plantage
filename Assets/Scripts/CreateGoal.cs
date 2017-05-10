bb478724e136dee021e89ccae128d38187864607
Essi Jukkala <essi.jukkala@aalto.fi>
2017-05-10 17:46:24 +0300

    goals change when completed

--- Assets/Scripts/CreateGoal.cs
+++ Assets/Scripts/CreateGoal.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class CreateGoal : MonoBehaviour {
 
 	public GameObject Flowers;
+	int l=0, f=0;
+	string c;
+
 	// Use this for initialization
 	void Start () {
 		int i = Random.Range(0, Flowers.GetComponent<PlayTurn>().prefabs.Length);
@@ -14,13 +17,21 @@ public class CreateGoal : MonoBehaviour {
 		Sprite leaf = goal.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite;
 		gameObject.transform.GetChild(0).GetComponent<Image>().sprite = flower;
 		gameObject.transform.GetChild(1).GetComponent<Image>().sprite = leaf;
+		c = goal.GetComponent<PlantInfo>().Color;
+		l = goal.GetComponent<PlantInfo>().LeafCorners;
+		f = goal.GetComponent<PlantInfo>().FlowerCorners;
 		Destroy(goal);
-
-
+		Debug.Log("Should be of color "+c+" with flower "+f+" and "+l);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		foreach(GameObject o in Flowers.GetComponent<PlayTurn>().flowersInGame){
+			if(o!=null && o.GetComponent<PlantInfo>().Color == c &&
+				o.GetComponent<PlantInfo>().FlowerCorners == f &&
+				o.GetComponent<PlantInfo>().LeafCorners == l){
+				Start();
+			}
+		}
 	}
 }
