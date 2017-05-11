9918b4bec4e49ba97f5a4cbd95086a870ad6db91
Essi Jukkala <essi.jukkala@aalto.fi>
2017-05-11 23:05:40 +0300

    updates

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BUTTON = 2099555730U;
+        static const AkUniqueID PLAY_FLOWER_DIE = 1604378720U;
+        static const AkUniqueID PLAY_FLOWER_MERGE = 869562106U;
+        static const AkUniqueID PLAY_GOAL = 83993043U;
+        static const AkUniqueID PLAY_MERGE_FAIL = 2757013549U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_SPAWN = 1012143543U;
+        static const AkUniqueID PLAY_SPINNING_WHEEL = 4182057884U;
+        static const AkUniqueID STOP_MUSIC = 2837384057U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID IN_GAME_SFX = 2292822258U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
