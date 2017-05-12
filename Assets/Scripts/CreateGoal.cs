9ee03a68ca7cd96c8a03f0eb1a03d36fc00b7c6d
Essi Jukkala <essi.jukkala@aalto.fi>
2017-05-12 14:42:23 +0300

    ready

--- Assets/Scripts/CreateGoal.cs
+++ Assets/Scripts/CreateGoal.cs
@@ -5,18 +5,17 @@ using UnityEngine.UI;
 
 public class CreateGoal : MonoBehaviour {
 
+	public GameObject[] goals;
 	public GameObject Flowers;
 	int l=0, f=0;
 	string c;
 
 	// Use this for initialization
 	void Start () {
-		int i = Random.Range(0, Flowers.GetComponent<PlayTurn>().prefabs.Length);
-		GameObject goal = Instantiate(Flowers.GetComponent<PlayTurn>().prefabs[i], new Vector2(0,0), Quaternion.identity);
-		Sprite flower = goal.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
-		Sprite leaf = goal.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite;
+		int i = Random.Range(0, goals.Length);
+		GameObject goal = Instantiate(goals[i], new Vector2(0,0), Quaternion.identity);
+		Sprite flower = goal.transform.GetComponent<SpriteRenderer>().sprite;
 		gameObject.transform.GetChild(0).GetComponent<Image>().sprite = flower;
-		gameObject.transform.GetChild(1).GetComponent<Image>().sprite = leaf;
 		c = goal.GetComponent<PlantInfo>().Color;
 		l = goal.GetComponent<PlantInfo>().LeafCorners;
 		f = goal.GetComponent<PlantInfo>().FlowerCorners;
